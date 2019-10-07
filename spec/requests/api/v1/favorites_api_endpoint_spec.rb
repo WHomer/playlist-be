733144d930d6a00267bf1b5bffbb6c5cc4fa732b
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-10-07 16:01:34 -0600

    Add initial favorites request spec

---
+++ spec/requests/api/v1/favorites_api_endpoint_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe 'Favorites API endpoint' do
+  before :each do
+    @user = create(:user)
+
+    @s1 = create(:song)
+    @s2 = create(:song)
+    @s3 = create(:song)
+    @s4 = create(:song)
+
+    @f1 = create(:user_song, user: @user, song: @s1)
+    @f2 = create(:user_song, user: @user, song: @s2)
+    @f3 = create(:user_song, user: @user, song: @s3)
+  end
+
+  it 'user can retrieve a single favorite by ID' do
+    get "/api/v1/favorites/#{@f1.id}"
+
+    favorite = JSON.parse(response.body, symbolize_names: true)[:data][:attributes]
+
+    expect(response).to be_successful
+
+    expect(favorite[:name]).to eq(@s1.name)
+    expect(favorite[:album]).to eq(@s1.album)
+    expect(favorite[:artist]).to eq(@s1.artist)
+    expect(favorite[:genre]).to eq(@s1.genre)
+    expect(favorite[:rating]).to eq(@s1.rating)
+  end
+end
