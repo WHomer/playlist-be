34dafe598229e0226cb9a6ca06d498af56210fb2
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-10-07 15:58:10 -0600

    Update User and Song factories with Faker data

--- spec/factories/songs.rb
+++ spec/factories/songs.rb
@@ -1,8 +1,10 @@
 FactoryBot.define do
   factory :song do
-    source_track_id { 1 }
-    name { "MyString" }
-    album { "MyString" }
-    artist { "MyString" }
+    source_track_id { |n| n }
+    name { Faker::Music::Phish.song }
+    album { Faker::Music.album }
+    artist { Faker::Music.band }
+    genre { Faker::Music.genre }
+    rating { rand(101) }
   end
 end
