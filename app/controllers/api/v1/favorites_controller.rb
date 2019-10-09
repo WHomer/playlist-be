f47108b951ac5af6c98d2f723d4762a44b95c9fa
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-10-09 07:54:58 -0600

    Add FavoritesController#set_favorite, #song_params and #create action with route

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -1,6 +1,24 @@
 class Api::V1::FavoritesController < ApplicationController
+  before_action :set_favorite, only: :show
+
+  def create
+    song = Song.find_or_create_by(song_params)
+    UserSong.create(song: song, user_id: params[:user_id])
+
+    render json: SongSerializer.new(song), status: 201
+  end
+
   def show
-    favorite = UserSong.find(params[:id])
-    render json: SongSerializer.new(Song.find(favorite.song_id))
+    render json: SongSerializer.new(Song.find(@favorite.song_id))
+  end
+
+  private
+
+  def song_params
+    params.permit(:source_track_id, :name, :artist, :album, :genre, :rating)
+  end
+
+  def set_favorite
+    @favorite = UserSong.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get '/favorites/:id', to: 'favorites#show'
+      resources :favorites, only: [:show, :create]
     end
   end
 end
