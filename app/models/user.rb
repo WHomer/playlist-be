e61a7670b6f62f2cd6384798614d9d8bd760c3c3
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-10-14 16:26:19 -0600

    Add validatable module to User model
    Co-Authored-By: William Homer <billyhomeriii@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: JWTBlacklist
 
   alias_attribute :favorites, :user_songs
