79790097a6c33db71d043efbebd23f4985b02f64
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-10-14 12:08:32 -0600

    Add Devise configuration to User model
    Co-Authored-By: William Homer <billyhomeriii@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :jwt_authenticatable, jwt_revocation_strategy: JWTBlacklist
+
   alias_attribute :favorites, :user_songs
 
   has_many :user_songs
