e4348c54536d8ee3116de2296a751fc9498de535
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-10-14 16:26:57 -0600

    Add RegistrationsController#configure_sign_up_params
    Co-Authored-By: William Homer <billyhomeriii@gmail.com>

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,6 @@
 class RegistrationsController < Devise::RegistrationsController
+  before_action :configure_sign_up_params, only: :create
+
   respond_to :json
 
   def create
@@ -6,5 +8,11 @@ class RegistrationsController < Devise::RegistrationsController
     resource.save
 
     render_resource(resource)
+  end
+
+  private
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
   end
 end
