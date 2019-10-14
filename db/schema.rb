db4dcb50d95958da865c8b31df8de92bc002a677
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-10-14 12:08:54 -0600

    Add JwtBlacklist table and additional columns to User table and migrate
    Co-Authored-By: William Homer <billyhomeriii@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_07_215541) do
+ActiveRecord::Schema.define(version: 2019_10_14_165632) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "jwt_blacklist", force: :cascade do |t|
+    t.string "jti", null: false
+    t.index ["jti"], name: "index_jwt_blacklist_on_jti"
+  end
 
   create_table "songs", force: :cascade do |t|
     t.integer "source_track_id"
@@ -41,6 +46,12 @@ ActiveRecord::Schema.define(version: 2019_10_07_215541) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "user_songs", "songs"
