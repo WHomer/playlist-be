fe30d781e300ea6777da956fa76249c0dee2b570
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-10-14 12:07:41 -0600

    Add Devise configurations and routes
    Co-Authored-By: William Homer <billyhomeriii@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  # root to: 'home#index'
+
+  devise_for :users,
+              path: '',
+              path_names: {
+                registration: 'sign_up'
+              },
+              controllers: {
+                sessions: 'sessions',
+                registrations: 'registrations'
+              }
+
   namespace :api do
     namespace :v1 do
       resources :favorites, only: [:show, :create, :destroy]
