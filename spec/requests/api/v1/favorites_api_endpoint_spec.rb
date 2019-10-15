6158cf58cd6e88596bcba7618f7ac2647b4d938d
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-10-15 11:39:38 -0600

    Add favorite request specs for unauthenticated users

--- spec/requests/api/v1/favorites_api_endpoint_spec.rb
+++ spec/requests/api/v1/favorites_api_endpoint_spec.rb
@@ -1,107 +1,169 @@
 require 'rails_helper'
 
 RSpec.describe 'Favorites API endpoint', type: :request do
-  before :each do
-    @user = create(:user, email: 'email@test.com', password: 'test123')
+  describe 'as an authenticated user' do
+    before :each do
+      @user = create(:user, email: 'email@test.com', password: 'test123')
 
-    @s1 = create(:song)
-    @s2 = create(:song)
-    @s3 = create(:song)
-    @s4 = create(:song)
+      @s1 = create(:song)
+      @s2 = create(:song)
+      @s3 = create(:song)
+      @s4 = create(:song)
 
-    @f1 = create(:user_song, user: @user, song: @s1)
-    @f2 = create(:user_song, user: @user, song: @s2)
+      @f1 = create(:user_song, user: @user, song: @s1)
+      @f2 = create(:user_song, user: @user, song: @s2)
 
-    sign_in @user
+      sign_in @user
+    end
+
+    it 'user can retrieve all favorites' do
+      get '/api/v1/favorites'
+
+      favorites = JSON.parse(response.body, symbolize_names: true)[:data]
+
+      expect(response.status).to eq(200)
+
+      expect(favorites.length).to eq(2)
+
+      song_1 = favorites[0][:attributes]
+      expect(song_1[:name]).to eq(@s1.name)
+      expect(song_1[:album]).to eq(@s1.album)
+      expect(song_1[:artist]).to eq(@s1.artist)
+      expect(song_1[:genre]).to eq(@s1.genre)
+      expect(song_1[:rating]).to eq(@s1.rating)
+
+      song_2 = favorites[1][:attributes]
+      expect(song_2[:name]).to eq(@s2.name)
+      expect(song_2[:album]).to eq(@s2.album)
+      expect(song_2[:artist]).to eq(@s2.artist)
+      expect(song_2[:genre]).to eq(@s2.genre)
+      expect(song_2[:rating]).to eq(@s2.rating)
+    end
+
+    it 'user can retrieve a single favorite by ID' do
+      get "/api/v1/favorites/#{@f1.id}"
+
+      favorite = JSON.parse(response.body, symbolize_names: true)[:data][:attributes]
+
+      expect(response.status).to eq(200)
+
+      expect(favorite[:name]).to eq(@s1.name)
+      expect(favorite[:album]).to eq(@s1.album)
+      expect(favorite[:artist]).to eq(@s1.artist)
+      expect(favorite[:genre]).to eq(@s1.genre)
+      expect(favorite[:rating]).to eq(@s1.rating)
+    end
+
+    it 'user receives a 404 error when no favorite is found to GET' do
+      get '/api/v1/favorites/not_found'
+
+      error = JSON.parse(response.body, symbolize_names: true)[:errors][0]
+
+      expect(response.status).to eq(404)
+
+      expect(error[:status]).to eq(404)
+      expect(error[:title]).to eq('Not Found')
+    end
+
+    it 'user can create a new favorite' do
+      params = {  source_track_id: @s3.source_track_id,
+                  name: @s3.name,
+                  artist: @s3.artist,
+                  album: @s3.album,
+                  rating: @s3.rating,
+                  user_id: @user.id
+                }
+
+      post '/api/v1/favorites', params: params
+
+      favorite = JSON.parse(response.body, symbolize_names: true)[:data][:attributes]
+
+      expect(response.status).to eq(201)
+
+      expect(favorite[:name]).to eq(@s3.name)
+      expect(favorite[:album]).to eq(@s3.album)
+      expect(favorite[:artist]).to eq(@s3.artist)
+      expect(favorite[:genre]).to eq(@s3.genre)
+      expect(favorite[:rating]).to eq(@s3.rating)
+
+      expect(@user.favorites.count).to eq(3)
+    end
+
+    it 'user can delete an existing favorite' do
+      delete "/api/v1/favorites/#{@f2.id}"
+
+      expect(response.status).to eq(204)
+
+      expect(@user.favorites.count).to eq(1)
+    end
+
+    it 'user receives a 404 error when no favorite is found to DELETE' do
+      delete "/api/v1/favorites/not_found"
+
+      error = JSON.parse(response.body, symbolize_names: true)[:errors][0]
+
+      expect(response.status).to eq(404)
+
+      expect(error[:status]).to eq(404)
+      expect(error[:title]).to eq('Not Found')
+    end
   end
 
-  it 'user can retrieve all favorites' do
-    get "/api/v1/favorites"
+  describe 'as an unauthenticated user' do
+    before :each do
+      @user = create(:user, email: 'email@test.com', password: 'test123')
 
-    favorites = JSON.parse(response.body, symbolize_names: true)[:data]
+      @s1 = create(:song)
+      @s2 = create(:song)
+      @s3 = create(:song)
+      @s4 = create(:song)
 
-    expect(response.status).to eq(200)
+      @f1 = create(:user_song, user: @user, song: @s1)
+      @f2 = create(:user_song, user: @user, song: @s2)
+    end
 
-    expect(favorites.length).to eq(2)
+    it 'user receives a 401 error when retrieving all favorites' do
+      get '/api/v1/favorites'
 
-    song_1 = favorites[0][:attributes]
-    expect(song_1[:name]).to eq(@s1.name)
-    expect(song_1[:album]).to eq(@s1.album)
-    expect(song_1[:artist]).to eq(@s1.artist)
-    expect(song_1[:genre]).to eq(@s1.genre)
-    expect(song_1[:rating]).to eq(@s1.rating)
-    
-    song_2 = favorites[1][:attributes]
-    expect(song_2[:name]).to eq(@s2.name)
-    expect(song_2[:album]).to eq(@s2.album)
-    expect(song_2[:artist]).to eq(@s2.artist)
-    expect(song_2[:genre]).to eq(@s2.genre)
-    expect(song_2[:rating]).to eq(@s2.rating)
-  end
+      error = JSON.parse(response.body, symbolize_names: true)[:errors][0]
 
-  it 'user can retrieve a single favorite by ID' do
-    get "/api/v1/favorites/#{@f1.id}"
+      expect(response.status).to eq(401)
 
-    favorite = JSON.parse(response.body, symbolize_names: true)[:data][:attributes]
+      expect(error[:status]).to eq(401)
+      expect(error[:title]).to eq('Unauthorized')
+    end
 
-    expect(response.status).to eq(200)
+    it 'user receives a 401 error when retreiving a single favorite by ID' do
+      get "/api/v1/favorites/#{@f1.id}"
 
-    expect(favorite[:name]).to eq(@s1.name)
-    expect(favorite[:album]).to eq(@s1.album)
-    expect(favorite[:artist]).to eq(@s1.artist)
-    expect(favorite[:genre]).to eq(@s1.genre)
-    expect(favorite[:rating]).to eq(@s1.rating)
-  end
+      error = JSON.parse(response.body, symbolize_names: true)[:errors][0]
 
-  it 'user receives a 404 error when no favorite is found to GET' do
-    get '/api/v1/favorites/not_found'
+      expect(response.status).to eq(401)
 
-    error = JSON.parse(response.body, symbolize_names: true)[:error]
+      expect(error[:status]).to eq(401)
+      expect(error[:title]).to eq('Unauthorized')
+    end
 
-    expect(response.status).to eq(404)
+    it 'user receives a 401 error when creating a favorite' do
+      post '/api/v1/favorites'
 
-    expect(error).to eq('Not found')
-  end
+      error = JSON.parse(response.body, symbolize_names: true)[:errors][0]
 
-  it 'user can create a new favorite' do
-    params = {  source_track_id: @s3.source_track_id,
-                name: @s3.name,
-                artist: @s3.artist,
-                album: @s3.album,
-                rating: @s3.rating,
-                user_id: @user.id
-              }
+      expect(response.status).to eq(401)
 
-    post '/api/v1/favorites', params: params
+      expect(error[:status]).to eq(401)
+      expect(error[:title]).to eq('Unauthorized')
+    end
 
-    favorite = JSON.parse(response.body, symbolize_names: true)[:data][:attributes]
+    it 'user receives a 401 error when deleting a favorite' do
+      delete "/api/v1/favorites/#{@f2.id}"
 
-    expect(response.status).to eq(201)
+      error = JSON.parse(response.body, symbolize_names: true)[:errors][0]
 
-    expect(favorite[:name]).to eq(@s3.name)
-    expect(favorite[:album]).to eq(@s3.album)
-    expect(favorite[:artist]).to eq(@s3.artist)
-    expect(favorite[:genre]).to eq(@s3.genre)
-    expect(favorite[:rating]).to eq(@s3.rating)
+      expect(response.status).to eq(401)
 
-    expect(@user.favorites.count).to eq(3)
-  end
-
-  it 'user can delete an existing favorite' do
-    delete "/api/v1/favorites/#{@f2.id}"
-
-    expect(response.status).to eq(204)
-
-    expect(@user.favorites.count).to eq(1)
-  end
-
-  it 'user receives a 404 error when no favorite is found to DELETE' do
-    delete "/api/v1/favorites/not_found"
-
-    error = JSON.parse(response.body, symbolize_names: true)[:error]
-
-    expect(response.status).to eq(404)
-
-    expect(error).to eq('Not found')
+      expect(error[:status]).to eq(401)
+      expect(error[:title]).to eq('Unauthorized')
+    end
   end
 end
