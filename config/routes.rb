28aaec38fdc9e738b9f7a8285177f40d76dd2b14
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-10-24 16:29:28 -0600

    Add PlaylistsController#index with route

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :favorites, except: [:new, :edit]
+      resources :playlists, only: :index
     end
   end
 end
